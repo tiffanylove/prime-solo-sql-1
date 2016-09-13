5bf15da68d242d1f2b7f961a62a1522cb1282d62
Millie Walsh <millie@primeacademy.io>
2016-09-13 14:08:12 -0500

    init commit

---
+++ solution.sql
@@ -0,0 +1,11 @@
+-- 0. Get all the users
+SELECT * FROM syntax_practice;
+-- 1. Get all users from Chicago.
+-- 2. Get all users with usernames that contain the letter a.
+-- 3. The bank is giving a new customer bonus! Update all records with an account balance of 0.00 and a transactions_attempted of 0. Give them a new account balance of 10.00.
+-- 4. Select all users that have attempted 9 or more transactions.
+-- 5. Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
+-- 6. Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
+-- 7. Get all users with account balances that are more than $100.
+-- 8. Add a new record.
+-- 9. The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
